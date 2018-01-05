a8e0ee487911a568d3fb894106859b435e3f919d
ishatserka <34316999+ishatserka@users.noreply.github.com>
2018-01-05 11:06:12 +0300

    Update server.py

--- MFTIweek5/server.py
+++ MFTIweek5/server.py
@@ -1,68 +1,185 @@
 import asyncio
 
-warehouse = dict()
 
-def run_server(host, port):
-    pass
+class Storage:
+    """Класс для хранения метрик в памяти процесса"""
 
-def get_data(key):
-    result = None
-    if "*" in key:
-        for key in warehouse:
-            for data in warehouse[key]:
-                result = f"{key} {data[0]} {data[1]}\n"
-    else:
-        for data in warehouse[key]:
-            result = f"{key} {data[0]} {data[1]}\n"
-            
-    return result
+    def __init__(self):
+        # используем словарь для хранения метрик
+        self._data = {}
 
-def put(data):
-    key, value, timestamp = data[4:].split(" ")
-    if key in warehouse:
-        warehouse[key].append((value, timestamp))
-    else:
-        warehouse[key] = [(value, timestamp)] 
-    return "ok\n\n"
+    def put(self, key, value, timestamp):
+        if key not in self._data:
+            self._data[key] = {}
 
-def get(key):
-    key = key[4:]
-    result = "ok\n"
-    result = f"{result}{get_data(key)}\n"
+        self._data[key][timestamp] = value
+
+    def get(self, key):
+        data = self._data
+
+        # вовзращаем нужную метрику если это не *
+        if key != "*":
+            data = {
+                key: data.get(key, {})
+            }
         
+        # для простоты мы храним метрики в обычном словаре и сортируем значения
+        # при каждом запросе, в реальном приложении следует выбрать другую
+        # структуру данных
+        result = {}
+        for key, timestamp_data in data.items():
+            result[key] = sorted(timestamp_data.items())
 
-"""
-put test_key 13.0 1503319739
-ok
+        return result
 
-get test_key 
-ok
-test_key 13.0 1503319739
-test_key 12.0 1503319740
- 
-"""
 
-async def handle_echo(reader, writer):
-    data = await reader.read(1024)
-    message = data.decode("utf-8")
-    result = None
-    if "get" in message:
-        result = get(message)
-    elif "put" in message:
-        result = put(message)
-    else:
-        result = "error\nwrong command\n\n"
-    writer.write(result.encode("utf-8"))
-    writer.close()
-    
-loop = asyncio.get_event_loop()
-coro = asyncio.start_server(handle_echo, "127.0.0.1", 10001, loop=loop)
-server = loop.run_until_complete(coro)
-try:
-    loop.run_forever()
-except KeyboardInterrupt:
+class ParseError(ValueError):
     pass
 
-server.close()
-loop.run_until_complete(server.wait_closed())
-loop.close()
+
+class Parser:
+    """Класс для реализации протокола"""
+    
+    def encode(self, responses):
+        """Преобразование ответа сервера в строку для передачи в сокет"""
+        rows = []
+        for response in responses:
+            if not response:
+                continue
+            for key, values in response.items():
+                for timestamp, value in values:
+                    rows.append(f"{key} {value} {timestamp}")
+
+        result = "ok\n"
+
+        if rows:
+            result += "\n".join(rows) + "\n"
+
+        return result + "\n"
+
+    def decode(self, data):
+        """Разбор команды для дальнейшего выполнения. Возвращает список команд для выполнения"""
+        parts = data.split("\n")
+        commands = []
+        for part in parts:
+            if not part:
+                continue
+
+            try:
+                method, params = part.strip().split(" ", 1)
+                if method == "put":
+                    key, value, timestamp = params.split()
+                    commands.append(
+                        (method, key, float(value), int(timestamp))
+                    )
+                elif method == "get":
+                    key = params
+                    commands.append(
+                        (method, key)
+                    )
+                else:
+                    raise ValueError("unknown method")
+            except ValueError:
+                raise ParseError("wrong command")
+
+        return commands
+
+
+class ExecutorError(Exception):
+    pass
+
+
+class Executor:
+    """Класс Executor реализует метод run, который знает как выполнять команды сервера"""
+    
+    def __init__(self, storage):
+        self.storage = storage
+
+    def run(self, method, *params):
+        if method == "put":
+            return self.storage.put(*params)
+        elif method == "get":
+            return self.storage.get(*params)
+        else:
+            raise ExecutorError("Unsupported method")
+
+
+class EchoServerClientProtocol(asyncio.Protocol):
+    """Класс для реализции сервера при помощи asyncio"""
+    
+    # Обратите внимание на то, что storage является атрибутом класса
+    # Объект self.storage для всех экземмпляров класса EchoServerClientProtocol
+    # будет являться одним и тем же объектом для хранения метрик.  
+    storage = Storage()
+
+    def __init__(self):
+        super().__init__()
+
+        self.parser = Parser()
+        self.executor = Executor(self.storage)
+        self._buffer = b''
+
+    def process_data(self, data):
+        """Обработка входной команды сервера"""
+        
+        # разбираем сообщения при помощи self.parser
+        commands = self.parser.decode(data)
+
+        # выполняем команды и запоминаем результаты выполнения
+        responses = []
+        for command in commands:
+            resp = self.executor.run(*command)
+            responses.append(resp)
+
+        # преобразовываем команды в строку
+        return self.parser.encode(responses)
+
+    def connection_made(self, transport):
+        self.transport = transport
+
+    def data_received(self, data):
+        """Метод data_received вызывается при получении данных в сокете"""
+        self._buffer += data
+        try:
+            decoded_data = self._buffer.decode()
+        except UnicodeDecodeError:
+            return
+      
+        # ждем данных, если команда не завершена символом \n
+        if not decoded_data.endswith('\n'):
+            return
+        
+        self._buffer = b''
+
+        try:
+            # обрабатываем поступивший запрос
+            resp = self.process_data(decoded_data)
+        except (ParseError, ExecutorError) as err:
+            # формируем ошибку, в случае ожидаемых исключений
+            self.transport.write(f"error\n{err}\n\n".encode())
+            return
+
+        # формируем успешный ответ
+        self.transport.write(resp.encode())
+
+
+def run_server(host, port)
+    loop = asyncio.get_event_loop()
+    coro = loop.create_server(
+        EchoServerClientProtocol,
+        host, port
+    )
+    server = loop.run_until_complete(coro)
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+
+    server.close()
+    loop.run_until_complete(server.wait_closed())
+    loop.close()
+
+
+if __name__ == "__main__":
+    # запуск сервера для тестирования
+    run_server('127.0.0.1', 8888)
